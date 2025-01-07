b9f1b3be7b4c338a20937d8c6a2cf1afcbaa8b55
suresh.b04 <suresh.b04@infosys.com>
2025-01-07 12:37:41 +0530

    added changes

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@
 #free of open source
 #and its independent platform like we can use in (linux,windows or mac)
 #pyhton is interpreter(line by line verification) and java is compailer (execute all line code after it will through the error)
+#lines to change
 
 a=0.1
 b=0.3
