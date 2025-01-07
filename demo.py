138276f9904652fd89a9ccfc9e885788e21984a3
suresh.b04 <suresh.b04@infosys.com>
2025-01-07 12:26:37 +0530

    changes add

---
+++ demo.py
@@ -0,0 +1,11 @@
+#Python is object oriented laugague
+#free of open source
+#and its independent platform like we can use in (linux,windows or mac)
+#pyhton is interpreter(line by line verification) and java is compailer (execute all line code after it will through the error)
+
+a=0.1
+b=0.3
+print(a+b==0.4)
+
+
+    
